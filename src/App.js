8a66ea9e3d1a87cd07b5aaf19816293155624194
Bharat verma <happy.19rj@gmail.com>
2022-09-14 12:19:47 +0530

    kuch bhi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+import Divider from "./Components/Divider";
+import Songs from "./Components/Songs";
+import Movies from "./Components/Movies";
+import Games from "./Components/Games";
+import Home from "./Components/Home";
+import LoginPop from './Components/LoginPop'
+
+import { Routes, Route } from "react-router-dom";
+import SignUp from "./Components/SignUp";
+
+export default class App extends Component {
+  render() {
+   
+    return (
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Login' element={<LoginPop/>}/>
+        <Route path='/games' element={<Games />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/songs' element={<Songs />} />
+        <Route path='SignUp' element={<SignUp/>}/>
+      </Routes>
+      
+    );
+  }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom'
+<link href="https://fonts.googleapis.com/css?family=BioRhyme" rel="stylesheet"></link>
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
